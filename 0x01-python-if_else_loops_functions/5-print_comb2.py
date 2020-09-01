f76b485ce7645b99c096809becc6c50669cf068a
mauroxcf <maurox03@gmail.com>
2020-09-01 21:06:22 +0000

    program that prints numbers from 0 to 99 firts digit and last

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for num in range(0, 99):
-    print("{:02d}".format(num))
+for num in range(0, 100):
+    if num == 99:
+        print("{:02d}".format(num))
+    if num < 99:
+        print("{:02d}, ".format(num), end='')

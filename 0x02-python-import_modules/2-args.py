d0481575c47b8f849b239c9e2e0bb5e9cb482222
mauroxcf <maurox03@gmail.com>
2020-09-03 17:53:41 +0000

    fixing code in 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,11 @@
 if __name__ == "__main__":
     import sys
     argv = sys.argv
-    print("{} arguments.".format(len(argv[1:])))
+    if len(argv[1:]) == 0:
+        print("{} arguments.".format(len(argv[1:])))
+    elif len(argv[1:]) == 1:
+        print("{} argument:".format(len(argv[1:])))
+    else:
+        print("{} arguments:".format(len(argv[1:])))
     for i in range(1, len(argv)):
-         print("{}: {}".format(i, argv[i]))
+        print("{}: {}".format(i, argv[i]))

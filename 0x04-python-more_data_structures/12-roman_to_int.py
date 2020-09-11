7231028640eb9b1914e00f2a98aab9287e1ba14e
mauroxcf <maurox03@gmail.com>
2020-09-11 03:43:36 +0000

    function converts roman numbers to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    dict_nroman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    nroman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     if type(roman_string) != str or roman_string is None:
         return 0
     for i in range(len(roman_string)):
-        if i > 0 and dict_nroman[roman_string[i]] > dict_nroman[roman_string[i - 1]]:
-            result += dict_nroman[roman_string[i]] - (dict_nroman[roman_string[i - 1]] * 2)
+        if i > 0 and nroman[roman_string[i]] > nroman[roman_string[i - 1]]:
+            result += nroman[roman_string[i]] - (nroman[roman_string[i - 1]] * 2)
         else:
-            result += dict_nroman[roman_string[i]]
+            result += nroman[roman_string[i]]
     return result

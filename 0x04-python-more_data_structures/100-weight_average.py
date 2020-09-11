ea247492146b33455a96c2bb5bfcb9c62798101b
mauroxcf <maurox03@gmail.com>
2020-09-11 22:14:46 +0000

    function that returns the weighted average of all integers tuple (<score>, <weight>)

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list:
+        sum_and_multi = (sum(map(lambda x: x[0] * x[1], my_list)))
+        pond = (sum(map(lambda x: x[1], my_list)))
+        return (sum_and_multi / pond)

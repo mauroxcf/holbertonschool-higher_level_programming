5ea65c6b7b24bc265e66e66a7e32d3d6741af1c7
mauroxcf <maurox03@gmail.com>
2020-09-11 03:28:09 +0000

    function that converts a Roman numeral to an integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    dict_nroman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    if type(roman_string) != str or roman_string is None:
+        return 0
+    for i in range(len(roman_string)):
+        if i > 0 and dict_nroman[roman_string[i]] > dict_nroman[roman_string[i - 1]]:
+            result += dict_nroman[roman_string[i]] - (dict_nroman[roman_string[i - 1]] * 2)
+        else:
+            result += dict_nroman[roman_string[i]]
+    return result

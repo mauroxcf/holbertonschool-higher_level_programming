2f15970debd2011389a2361e583ffdb7bcd81e3f
mauroxcf <maurox03@gmail.com>
2020-09-22 21:04:13 +0000

    a class Square that defines a square by: (based on 1-square.py)

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+""" Square Estructure"""
+
+
+class Square:
+    """ Class represent a square """
+    def __init__(self, size=0):
+        """ condition to evaluate if size its a negative or a int attribute """
+        if size is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        # self.__size = size

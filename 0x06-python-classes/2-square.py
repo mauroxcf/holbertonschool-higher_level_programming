252c80b2235befbc4484fd89f22a27e0aaa88dfc
mauroxcf <maurox03@gmail.com>
2020-09-22 21:12:44 +0000

    a class Square that defines a square by: (based on 1-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,6 +10,6 @@ class Square:
         """ condition to evaluate if size its a negative or a int attribute """
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        # self.__size = size
+        self.__size = size

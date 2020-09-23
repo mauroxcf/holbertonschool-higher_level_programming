ae20ea51c95e87c5935140e575673558a00b200d
mauroxcf <maurox03@gmail.com>
2020-09-23 03:10:11 +0000

    ByteCode -> Python #5

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-import dis
 import math
-
 class MagicClass:
     def __init__(self, raidus=0):
         if type(radius) is not int or type(radius) is not float:
@@ -13,6 +11,3 @@ class MagicClass:
 
     def circumference(self):
         return (2 * math.pi) * self.__radius
-
-
-dis.dis(MagicClass)

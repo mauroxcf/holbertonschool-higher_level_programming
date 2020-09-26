ee784f52c07356864c2dc983a1b1fade4072bdc6
mauroxcf <maurox03@gmail.com>
2020-09-26 02:42:11 +0000

    Adding Readme file, tests directory and test 0, and function that adds 2 integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Program that add 2 integers """
+
+def add_integer(a, b=98):
+    """
+    a and b must be integers and if its not, make a cast
+    """
+    if type(a) is int or type(a) is float:
+        a = int(a)
+    else:
+        raise TypeError("a must be an integer")
+    if type(b) is int or type(b) is float:
+        b = int(b)
+    else:
+        raise TypeError("b must be an integer")
+    return a + b

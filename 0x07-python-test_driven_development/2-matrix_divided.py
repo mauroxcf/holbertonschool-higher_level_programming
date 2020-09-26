e430f894629eabcf81156ecfa0101799ef9d543f
mauroxcf <maurox03@gmail.com>
2020-09-26 21:20:54 +0000

    function that divides all elements of a matrix and fixing pep8, codefix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,29 +6,34 @@ def matrix_divided(matrix, div):
     """
     matrix its a list type with a sublist of numbers
     """
+    horrors = {
+        'mlist': 'matrix must be a matrix (list of lists) of integers/floats',
+        'rowlist': 'Each row of the matrix must have the same size',
+        'div_int': 'div must be a number',
+        'zero_div': 'division by zero'
+    }
+
     if type(matrix) is list:
-        new_list = []
+        newl = []
         row_list = None
         for sub_list in matrix:
             if type(sub_list) is list:
                 if row_list is None:
                     row_list = len(sub_list)
-                    for elemt_list in sub_list:
-                        if type(elemt_list) is int or type(elemt_list) is float:
-                            continue
-                        else:
-                            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
                 elif row_list != len(sub_list):
-                    raise TypeError("Each row of the matrix must have the same size")
+                    raise TypeError(horrors['rowlist'])
+                for elemtl in sub_list:
+                    if type(elemtl) is not int and type(elemtl) is not float:
+                        raise TypeError(horrors['mlist'])
             else:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(horrors['mlist'])
     else:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(horrors['mlist'])
 
     if type(div) is not int and type(div) is not float:
-        raise TypeError("div must be a number")
+        raise TypeError(horrors['div_int'])
     if div is 0:
-        raise ZeroDivisionError("division by zero")
+        raise ZeroDivisionError(horrors['zero_div'])
 
-    new_list = list(map(lambda x: list(map(lambda x: round(x / div, 2), x)), matrix))
-    return new_list
+    newl = map(lambda x: list(map(lambda x: round(x / div, 2), x)), matrix)
+    return list(newl)

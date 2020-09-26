e6673d3638b40cf124607c088c007f9e84d8953c
mauroxcf <maurox03@gmail.com>
2020-09-26 22:38:17 +0000

    function that divides all elements of a matrix, FIXING ANYTHING

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,10 +7,10 @@ def matrix_divided(matrix, div):
     matrix its a list type with a sublist of numbers
     """
     horrors = {
-        'mlist': 'matrix must be a matrix (list of lists) of integers/floats',
-        'rowlist': 'Each row of the matrix must have the same size',
-        'div_int': 'div must be a number',
-        'zero_div': 'division by zero'
+        "mlist": "matrix must be a matrix (list of lists) of integers/floats",
+        "rowlist": "Each row of the matrix must have the same size",
+        "div_int": "div must be a number",
+        "zero_div": "division by zero"
     }
 
     if type(matrix) is list and matrix != []:
@@ -20,20 +20,20 @@ def matrix_divided(matrix, div):
             if type(sub_list) is list and sub_list != []:
                 if row_list is None:
                     row_list = len(sub_list)
-                elif row_list != len(sub_list):
-                    raise TypeError(horrors['rowlist'])
+                if row_list != len(sub_list):
+                    raise TypeError(horrors["rowlist"])
                 for elemtl in sub_list:
                     if type(elemtl) is not int and type(elemtl) is not float:
-                        raise TypeError(horrors['mlist'])
+                        raise TypeError(horrors["mlist"])
             else:
-                raise TypeError(horrors['mlist'])
+                raise TypeError(horrors["mlist"])
     else:
-        raise TypeError(horrors['mlist'])
+        raise TypeError(horrors["mlist"])
 
     if type(div) is not int and type(div) is not float:
-        raise TypeError(horrors['div_int'])
+        raise TypeError(horrors["div_int"])
     if div is 0:
-        raise ZeroDivisionError(horrors['zero_div'])
+        raise ZeroDivisionError(horrors["zero_div"])
 
     nw = list(map(lambda x: list(map(lambda x: round(x / div, 2), x)), matrix))
     return nw

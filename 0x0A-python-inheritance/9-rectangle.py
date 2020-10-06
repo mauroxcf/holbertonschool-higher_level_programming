9ba067e69318d6beef57b107403b145656c4297b
mauroxcf <maurox03@gmail.com>
2020-10-06 16:43:27 +0000

    9. Full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Base Geometry class """
+
+class BaseGeometry:
+    """
+    adding area and integer validator
+    """
+    def area(self):
+        if self:
+            raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        integer validator
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value < 0 or value == 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """ sub class rectangle """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ area calculation """
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+    #implementar el metodo area... hay qye volver a escribirlo e implementar el metodo magico __str__
+    #para imprimir

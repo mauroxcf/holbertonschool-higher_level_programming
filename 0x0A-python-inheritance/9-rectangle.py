656ed2ced7e300d4efc978f633110335f2a9975d
mauroxcf <maurox03@gmail.com>
2020-10-06 23:59:32 +0000

    9. Full rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -19,6 +19,7 @@ class BaseGeometry:
         if value < 0 or value == 0:
             raise ValueError("{} must be greater than 0".format(name))
 
+
 class Rectangle(BaseGeometry):
     """ sub class rectangle """
     def __init__(self, width, height):

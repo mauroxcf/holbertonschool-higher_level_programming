73e1b035b998f6166598117016dccbb5628af69b
mauroxcf <maurox03@gmail.com>
2020-10-07 21:55:34 +0000

    adding doc

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ def read_file(filename=""):
     """
     read and print a file
     """
-    with open(filename, "r") as reader:
-        print(reader.read())
+    with open(filename) as reader:
+        print(reader.read(), end="")

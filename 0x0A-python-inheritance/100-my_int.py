ff461af78f36718fdd7d8000847c907114dab643
mauroxcf <maurox03@gmail.com>
2020-10-07 02:09:55 +0000

    Write a class MyList that inherits from list

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" MyInt class rebel """
+
+
+class MyInt(int):
+    """ Rebel """
+    def __init__(self, value=0):
+        """ Constructor """
+        self.__value = value
+
+    def __eq__(self, value):
+        """ invert equal """
+        return not (self.__value is not value)
+
+    def __ne__(self, value):
+        """ invert negation """
+        return not (self.__value is value)

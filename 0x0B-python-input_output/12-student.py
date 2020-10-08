60f04ee478e8342594de64626b1089fe7f13d556
mauroxcf <maurox03@gmail.com>
2020-10-08 00:52:34 +0000

    12. Student to JSON with filter

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Class Student """
+
+
+class Student:
+    """
+    Student info
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        constructor
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        class object type
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for i in self.__dict__:
+                if i in attrs:
+                    new_dict[i] = self.__dict__[i]
+            return new_dict

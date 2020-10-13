811dca53c102d8ed0361869ebcd5ada0d414faab
mauroxcf <maurox03@gmail.com>
2020-10-13 17:39:52 +0000

    fixing pep8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -96,6 +96,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """ returns the dictionary representation of a Rectangle """
-        dicto = {"x": self.x, "y": self.y, "id": self.id,
+        return {"x": self.x, "y": self.y, "id": self.id,
          "height": self.height, "width": self.width}
-        return dicto

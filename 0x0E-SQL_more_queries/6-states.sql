bc0ed7d01e6bcb49f5107d6d53a60c2067fb70bb
mauroxcf <maurox03@gmail.com>
2020-11-17 23:52:22 +0000

    script that creates the database hbtn_0d_usa and the table states with id unique, auto generated, cant be null and is a primary key

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- script that creates the database and a table with some specify info
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-name VARCHAR(256))
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+name VARCHAR(256) NOT NULL);

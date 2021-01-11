68cd5c49eb7d381daef2ec6352a25342a5608454
mauroxcf <maurox03@gmail.com>
2021-01-11 14:09:01 -0500

    Python script that fetches https://intranet.hbtn.io/status with requests module

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Python script that fetches https://intranet.hbtn.io/status with request module
+""" Using Request module """
+
+
+if __name__ == '__main__':
+    import requests as reqs
+
+    response = reqs.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
+    print("\t- utf8 content: {}".format(response.encoding))

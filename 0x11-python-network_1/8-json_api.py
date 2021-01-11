662d22c9c62b129cde0f3ad99a3b3867d135579e
mauroxcf <maurox03@gmail.com>
2021-01-11 16:46:34 -0500

    8. Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# sends a POST request to a URL with a parameter using request module
+""" Using Request module """
+
+
+if __name__ == '__main__':
+    import sys
+    import requests as reqs
+
+    url = "http://0.0.0.0:5000/search_user"
+
+    if len(sys.argv) > 1:
+        data = {'q': sys.argv[1]}
+    else:
+        data = {'q': ""}
+
+    response = reqs.post(url, data)
+    resjson = response.json()
+
+    if len(resjson) > 0:
+        if 'id' in resjson and 'name' in resjson:
+            print('[{}] {}'.format(resjson.get('id'), resjson.get('name')))
+        else:
+            print('Not a valid JSON')
+    else:
+        print('No result')

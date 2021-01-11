b99790d68e072eb0117a5b56a1cc3eb8d8738cef
mauroxcf <maurox03@gmail.com>
2021-01-11 15:13:26 -0500

    Python script that takes in a URL, sends a request to the URL and displays the body of the response using Request module and dealing http errors code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# sends a POST request to a URL with a parameter using request module
+""" Using Request module """
+
+
+if __name__ == '__main__':
+    import sys
+    import requests as reqs
+
+    response = reqs.get(sys.argv[1])
+
+    if response.status_code >= 200:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)

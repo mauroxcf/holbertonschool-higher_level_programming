57d146b2d264a2eda3452e1aea0d40a8f85a2f6d
mauroxcf <maurox03@gmail.com>
2021-01-11 14:20:59 -0500

    sends a request to the URL and displays the value of the variable X-Request-Id using request module

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# sends a request to the URL and displays X-Request-Id using request module
+""" Using Request module """
+
+
+if __name__ == '__main__':
+    import sys
+    import requests as reqs
+
+    response = reqs.get(sys.argv[1])
+    print(response.headers['X-Request-Id'])

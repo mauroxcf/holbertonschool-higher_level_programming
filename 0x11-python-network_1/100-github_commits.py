f8c78d31e1ed75a96ed5b1a7cb743e14b2df5940
mauroxcf <maurox03@gmail.com>
2021-01-11 22:43:12 -0500

    list 10 commits of the repository x by the user x using the Github API

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Using Request module """
+
+
+if __name__ == '__main__':
+    import requests as reqs
+    from requests.auth import HTTPBasicAuth
+    from sys import argv
+
+    repository = argv[1]
+    user = argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(user, repository)
+    response = reqs.get(url).json()
+    for i in response[:10]:
+        print("{}: {}".format(i.get('sha'), i.get('commit').get('author').get('name')))
